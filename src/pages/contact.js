2f5ab13ea090f4553333e2387ce2cd8222dc7a83
Ryan <ryanpburnett@yahoo.com>
2021-04-06 12:11:35 -0500

    pages folder and starter code added

---
+++ src/pages/contact.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Contact = () => {
+    <p>contact</p>
+}
+
+export default Contact
---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Index = () => {
+    <p>index</p>
+}
+
+export default Index
