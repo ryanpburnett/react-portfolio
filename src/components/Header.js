7a9d40daecb0b12872f2814685fb2e252974250c
Ryan <ryanpburnett@yahoo.com>
2021-04-06 13:59:32 -0500

    added bootstrap

--- src/components/Header.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Header(props) {
-    return(
-        <p>header</p>
-    )
-}
-
-export default Header;
---
+++ src/components/Navbar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Navbar(props) {
+    return(
+        <p>navbar</p>
+    )
+}
+
+export default Navbar;
